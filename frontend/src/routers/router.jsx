56fa84e43db48edeafe6a1155f9fc79aa446a97e
alejo <alejo.cagliolo@outlook.com>
2025-01-04 11:33:55 -0300

    Admin login

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -8,6 +8,7 @@ import CheckoutPage from "../pages/books/CheckoutPage";
 import SingleBook from "../pages/books/SingleBook";
 import PrivateRoute from "./PrivateRoute";
 import OrderPage from "../pages/books/OrderPage";
+import AdminRoute from "./AdminRoute";
 
 const router = createBrowserRouter([
   {
@@ -47,5 +48,30 @@ const router = createBrowserRouter([
         
     ]
   },
+  {
+    path: "/admin",
+    element: <div>Admin Login</div>
+  },
+  {
+    path: "/dashboard",
+    element: <AdminRoute> <div>Admin Dashboard</div></AdminRoute>,
+    children: [{
+      path: "",
+      element: <AdminRoute><div>Dashboard Home</div> </AdminRoute>
+    }, 
+    {
+      path: "add-new-book",
+      element:<AdminRoute> <div>Add New Book</div></AdminRoute>
+    },
+    {
+      path: "edit-book/:id",
+      element:<AdminRoute> <div>Edit Book</div></AdminRoute>
+    },
+    {
+      path: "manage-books",
+      element:<AdminRoute> <div>Manage Books</div></AdminRoute>
+    },
+  ]
+  }
 ]);
 export default router;
