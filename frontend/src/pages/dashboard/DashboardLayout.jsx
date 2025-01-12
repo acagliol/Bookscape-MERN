70d1e8705b2498922bb0ca60d9fc30a2ce596117
alejo <alejo.cagliolo@outlook.com>
2025-01-12 14:22:39 -0600

    Dashboard Layout functions

--- frontend/src/pages/dashboard/DashboardLayout.jsx
+++ frontend/src/pages/dashboard/DashboardLayout.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import getBaseURL from "../../utils/baseUrl";
 
 const DashboardLayout = () => {
-  return (
-    <div>DashboardLayout</div>
-  )
-}
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState({});
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${getBaseURL()}/api/admin`, {
+          headers: {
+            'Authorization': `Bearer ${localStorage.getItem("token")}}`,
+            'Content-Type': 'application/json',
+          },
 
-export default DashboardLayout
+        });
+        setData(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error:", error);
+        setLoading(false);
+      }
+    }
+  }, []);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return <div>DashboardLayout</div>;
+};
+
+export default DashboardLayout;
