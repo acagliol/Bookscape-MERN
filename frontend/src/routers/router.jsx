03b38d7b5e83c2a61b6ce839f14ed156d011e66f
alejo <alejo.cagliolo@outlook.com>
2025-01-13 23:22:00 -0500

    Router dash

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -11,6 +11,7 @@ import OrderPage from "../pages/books/OrderPage";
 import AdminRoute from "./AdminRoute";
 import AdminLogin from "../components/AdminLogin";
 import DashboardLayout from "../pages/dashboard/DashboardLayout";
+import Dashboard from "../pages/dashboard/Dashboard";
 
 const router = createBrowserRouter([
   {
@@ -74,7 +75,7 @@ const router = createBrowserRouter([
         path: "",
         element: (
           <AdminRoute>
-            <div>Dashboard Home</div>{" "}
+            <Dashboard/>
           </AdminRoute>
         ),
       },
