ca9067b53771884559ed498d7eb4b9ee3144560d
alejo <alejo.cagliolo@outlook.com>
2025-01-13 23:38:35 -0500

    chart

--- frontend/src/pages/dashboard/RevenueChart.jsx
+++ frontend/src/pages/dashboard/RevenueChart.jsx
@@ -44,7 +44,7 @@ const RevenueChart = () => {
   return (
     <div className="w-full max-w-3xl mx-auto p-4 bg-white shadow-lg rounded-lg">
       <h2 className="text-center text-2xl font-bold text-gray-800 mb-4">Monthly Revenue</h2>
-      <div className='hidden md:block'>
+      <div className='md:block'>
       <Bar data={data} options={options} className='' />
       </div>
     </div>
