8a03b3f856f9d221e3a6b66723637fa7f0750f70
alejo <alejo.cagliolo@outlook.com>
2025-01-14 23:40:58 -0500

    updating dashboard layout

--- frontend/src/pages/dashboard/DashboardLayout.jsx
+++ frontend/src/pages/dashboard/DashboardLayout.jsx
@@ -1,19 +1,22 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import Loading from "../../components/Loading";
-import { Link, Outlet, useNavigate } from "react-router-dom";
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import Loading from '../../components/Loading';
+import { Link, Outlet, useNavigate } from 'react-router-dom';
 import { HiViewGridAdd } from "react-icons/hi";
-import { MdIncompleteCircle, MdOutlineManageHistory } from "react-icons/md";
+import { MdOutlineManageHistory } from "react-icons/md";
 
 const DashboardLayout = () => {
- 
+  
+  const navigate = useNavigate()
   const handleLogout = () => {
+    localStorage.removeItem('token');
+    navigate("/")
   }
- 
+
   return (
     <section className="flex md:bg-gray-100 min-h-screen overflow-hidden">
-    <aside className="sm:flex sm:flex-col">
-      <a href="#" className="inline-flex items-center justify-center h-20 w-20 bg-purple-600 hover:bg-purple-500 focus:bg-purple-500">
+    <aside className=" sm:flex sm:flex-col">
+      <a href="/" className="inline-flex items-center justify-center h-20 w-20 bg-purple-600 hover:bg-purple-500 focus:bg-purple-500">
         <img src="/fav-icon.png" alt="" />
       </a>
       <div className="flex-grow flex flex-col justify-between text-gray-500 bg-gray-800">
@@ -24,17 +27,17 @@ const DashboardLayout = () => {
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
             </svg>
           </a>
-          <Link to="#" className="inline-flex items-center justify-center py-3 text-purple-600 bg-white rounded-lg">
+          <Link to="/dashboard" className="inline-flex items-center justify-center py-3 text-purple-600 bg-white rounded-lg">
             <span className="sr-only">Dashboard</span>
             <svg aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
             </svg>
           </Link>
-          <Link to="#" className="inline-flex items-center justify-center py-3 hover:text-gray-400 hover:bg-gray-700 focus:text-gray-400 focus:bg-gray-700 rounded-lg">
+          <Link to="/dashboard/add-new-book" className="inline-flex items-center justify-center py-3 hover:text-gray-400 hover:bg-gray-700 focus:text-gray-400 focus:bg-gray-700 rounded-lg">
             <span className="sr-only">Add Book</span>
             <HiViewGridAdd className="h-6 w-6"/>
           </Link>
-          <Link to="#" className="inline-flex items-center justify-center py-3 hover:text-gray-400 hover:bg-gray-700 focus:text-gray-400 focus:bg-gray-700 rounded-lg">
+          <Link to="/dashboard/manage-books" className="inline-flex items-center justify-center py-3 hover:text-gray-400 hover:bg-gray-700 focus:text-gray-400 focus:bg-gray-700 rounded-lg">
             <span className="sr-only">Documents</span>
             <MdOutlineManageHistory className="h-6 w-6"/>
           </Link>
@@ -79,9 +82,7 @@ const DashboardLayout = () => {
             </svg> 
           </button>
           <div className="border-l pl-3 ml-3 space-x-1">
-            <button 
-            onClick={handleLogout}
-            className="relative p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-600 focus:bg-gray-100 focus:text-gray-600 rounded-full">
+            <button className="relative p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-600 focus:bg-gray-100 focus:text-gray-600 rounded-full">
               <span className="sr-only">Notifications</span>
               <span className="absolute top-0 right-0 h-2 w-2 mt-1 mr-2 bg-red-500 rounded-full"></span>
               <span className="absolute top-0 right-0 h-2 w-2 mt-1 mr-2 bg-red-500 rounded-full animate-ping"></span>
@@ -90,7 +91,7 @@ const DashboardLayout = () => {
               </svg>
             </button>
             <button
-            
+            onClick={handleLogout}
             className="relative p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-600 focus:bg-gray-100 focus:text-gray-600 rounded-full">
               <span className="sr-only">Log out</span>
               <svg aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
@@ -113,7 +114,7 @@ const DashboardLayout = () => {
               </svg>
               Manage Books
             </Link>
-            <Link to="#" className="inline-flex px-5 py-3 text-white bg-purple-600 hover:bg-purple-700 focus:bg-purple-700 rounded-md ml-6 mb-3">
+            <Link to="/dashboard/add-new-book" className="inline-flex px-5 py-3 text-white bg-purple-600 hover:bg-purple-700 focus:bg-purple-700 rounded-md ml-6 mb-3">
               <svg aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="flex-shrink-0 h-6 w-6 text-white -ml-1 mr-2">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
               </svg>
@@ -125,7 +126,7 @@ const DashboardLayout = () => {
       </main>
     </div>
   </section>
-  );
-};
+  )
+}
 
-export default DashboardLayout;
+export default DashboardLayout
