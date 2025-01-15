e65d5e035a6c26ff91d8e9cc3a64c7a49b3d6f60
alejo <alejo.cagliolo@outlook.com>
2025-01-14 23:27:40 -0500

    yup

--- frontend/src/pages/dashboard/manageBooks/ManageBooks.jsx
+++ frontend/src/pages/dashboard/manageBooks/ManageBooks.jsx
@@ -1,6 +1,30 @@
 import React from 'react'
+import { useFetchAllBooksQuery } from '../../../redux/features/books/bookApi';
 
 const ManageBooks = () => {
+    const navigate = useNavigate();
+
+    const {data: books, refetch} = useFetchAllBooksQuery()
+
+    const [deleteBook] = useDeleteBookMutation()
+
+    // Handle deleting a book
+    const handleDeleteBook = async (id) => {
+        try {
+            await deleteBook(id).unwrap();
+            alert('Book deleted successfully!');
+            refetch();
+
+        } catch (error) {
+            console.error('Failed to delete book:', error.message);
+            alert('Failed to delete book. Please try again.');
+        }
+    };
+
+    // Handle navigating to Edit Book page
+    const handleEditClick = (id) => {
+        navigate(`dashboard/edit-book/${id}`);
+    };
   return (
     <div>ManageBooks</div>
   )
