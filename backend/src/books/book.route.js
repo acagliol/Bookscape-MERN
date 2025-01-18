d25dd0c9386792c03998c63ff07d104e8adce09b
alejo <alejo.cagliolo@outlook.com>
2025-01-18 12:55:50 -0500

    bugs

--- backend/src/books/book.route.js
+++ backend/src/books/book.route.js
@@ -1,37 +1,28 @@
-
 const express = require('express');
 const Book = require('./book.model');
 const { postABook, getAllBooks, getSingleBook, UpdateBook, deleteABook } = require('./book.controller');
 const verifyAdminToken = require('../middleware/verifyAdminToken');
-const router = express.Router();
+const router =  express.Router();
 
-//frontend => backend server => controller => book schema => db => send data to server => frontend
-
+// frontend => backend server => controller => book schema  => database => send to server => back to the frontend
+//post = when submit something fronted to db
+// get =  when get something back from db
+// put/patch = when edit or update something
+// delete = when delete something
 
 // post a book
-// post = when submit something frontend to db
-// get = when get something back from db
-// put/patch = when you edit or update something
-// delete = when delete from db
-
-
-router.post("/create-book", verifyAdminToken, postABook);
+router.post("/create-book", verifyAdminToken, postABook)
 
 // get all books
-router.get("/",getAllBooks);
+router.get("/", getAllBooks);
 
-// get single book
-
+// single book endpoint
 router.get("/:id", getSingleBook);
 
-// update book
-
+// update a book endpoint
 router.put("/edit/:id", verifyAdminToken, UpdateBook);
 
-// delete book
-
-router.delete("/:id", verifyAdminToken, deleteABook); 
-
+router.delete("/:id", verifyAdminToken, deleteABook)
 
 
 module.exports = router;
--- frontend/src/redux/features/books/bookApi.js
+++ frontend/src/redux/features/books/bookApi.js
@@ -1,13 +1,12 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import getBaseUrl from '../../../utils/baseUrl';
+import getBaseUrl from '../../../utils/baseURL'
 
-
-const baseQuery = fetchBaseQuery({
+const  baseQuery = fetchBaseQuery({
     baseUrl: `${getBaseUrl()}/api/books`,
     credentials: 'include',
     prepareHeaders: (Headers) => {
-        const token = localStorage.getItem('token');
-        if (token) {
+        const token =  localStorage.getItem('token');
+        if(token) {
             Headers.set('Authorization', `Bearer ${token}`);
         }
         return Headers;
@@ -15,46 +14,46 @@ const baseQuery = fetchBaseQuery({
 })
 
 const booksApi = createApi({
-    reducerPath: 'bookApi',
+    reducerPath: 'booksApi',
     baseQuery,
-    endpoints: (builder) => ({
+    tagTypes: ['Books'],
+    endpoints: (builder) =>({
         fetchAllBooks: builder.query({
-            query: () => '/',
+            query: () => "/",
             providesTags: ["Books"]
         }),
         fetchBookById: builder.query({
             query: (id) => `/${id}`,
-            providesTags: (results, error ,id) => [{type: "Books", id}], 
-
+            providesTags: (result, error, id) => [{ type: "Books", id }],
         }),
         addBook: builder.mutation({
             query: (newBook) => ({
                 url: `/create-book`,
                 method: "POST",
-                body: newBook,
+                body: newBook
             }),
             invalidatesTags: ["Books"]
         }),
-        updateBook: builder.mutation ({
-            query: (id, ...rest) => ({
+        updateBook: builder.mutation({
+            query: ({id, ...rest}) => ({
                 url: `/edit/${id}`,
                 method: "PUT",
                 body: rest,
-                header: {
-                    'Content-Type': 'application/json',
+                headers: {
+                    'Content-Type': 'application/json'
                 }
-            }), 
+            }),
             invalidatesTags: ["Books"]
         }),
         deleteBook: builder.mutation({
             query: (id) => ({
-                url: `/delete/${id}`,
-                method: "DELETE",
-            }), 
+                url: `/${id}`,
+                method: "DELETE"
+            }),
             invalidatesTags: ["Books"]
         })
     })
-});
+})
 
-export const { useFetchAllBooksQuery, useFetchBookByIdQuery, useAddBookMutation, useDeleteBookMutation, useUpdateBookMutation } = booksApi;
+export const {useFetchAllBooksQuery, useFetchBookByIdQuery, useAddBookMutation, useUpdateBookMutation, useDeleteBookMutation} = booksApi;
 export default booksApi;
