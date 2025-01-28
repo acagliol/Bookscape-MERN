d3240f5194a48f8422f1547de5255f9e077d3213
alejo <alejo.cagliolo@outlook.com>
2025-01-27 23:35:56 -0500

    Bugs on updating books

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import Loading from '../../components/Loading';
-import getBaseURL from '../../utils/baseUrl';
+import getBaseURL from '../../utils/baseURL';
 import { MdIncompleteCircle } from 'react-icons/md';
 import RevenueChart from './RevenueChart';
 
