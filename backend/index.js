67ebedec4c074eb197fb09e1811fa19bde87466b
alejo <alejo.cagliolo@outlook.com>
2025-01-28 00:00:40 -0500

    Updating vercel

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173', 'https://bookscape-mern.vercel.app/'],
   credentials: true,
 }))
 
