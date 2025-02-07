93cd2f65a344ab12463b574f8d44a365166e1b39
alejo <alejo.cagliolo@outlook.com>
2025-02-06 22:19:58 -0500

    Fixing admin login

--- frontend/src/components/AdminLogin.jsx
+++ frontend/src/components/AdminLogin.jsx
@@ -1,48 +1,52 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import axios from "axios";
-import getBaseURL from "../utils/baseURL";
+import getBaseUrl from "../utils/baseURL";
+import { useNavigate } from "react-router-dom";
+import { FaShieldAlt } from "react-icons/fa";
 
-const Login = () => {
+const AdminLogin = () => {
   const [message, setMessage] = useState("");
-  const navigate = useNavigate();
-
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
 
+  const navigate = useNavigate();
+
   const onSubmit = async (data) => {
-    console.log(data);
     try {
-      const response = await axios.post(`${getBaseURL()}/api/auth/admin`, data, {
+      const response = await axios.post(`${getBaseUrl()}/api/auth/admin`, data, {
         headers: {
           "Content-Type": "application/json",
         },
       });
       const auth = response.data;
-      console.log(auth);
+
       if (auth.token) {
         localStorage.setItem("token", auth.token);
         setTimeout(() => {
           localStorage.removeItem("token");
-          alert("Session expired, please login again.");
+          alert("Token has expired! Please login again.");
           navigate("/");
         }, 3600 * 1000);
       }
-      alert("Admin Login successful");
+
+      alert("Admin Login successful!");
       navigate("/dashboard");
     } catch (error) {
-      setMessage("Invalid email or password");
+      setMessage("Please provide a valid username and password");
+      console.error(error);
     }
   };
 
   return (
-    <div className="h-screen flex justify-center items-center bg-[#FFF8E1]">
-      <div className="w-full max-w-sm mx-auto bg-[#FDFCEB] shadow-md rounded-lg px-8 pt-6 pb-9">
-        <h2 className="text-2xl font-semibold text-[#9B1C1C] mb-4 text-center">Admin Login</h2>
+    <div className="h-screen flex justify-center items-center bg-[#FFF8E1] text-[#9B1C1C]">
+      <div className="w-full max-w-sm mx-auto bg-white shadow-md rounded-lg px-8 pt-6 pb-9">
+        <h2 className="text-2xl font-bold mb-6 text-center flex items-center justify-center">
+          <FaShieldAlt className="mr-2" /> Admin Dashboard Login
+        </h2>
 
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-4">
@@ -53,10 +57,9 @@ const Login = () => {
               {...register("username", { required: true })}
               type="text"
               id="username"
-              placeholder="Enter username"
-              className="shadow appearance-none border border-[#9B1C1C] rounded w-full py-2 px-3 leading-tight focus:outline-none focus:ring-1 focus:ring-[#9B1C1C]"
+              placeholder="Enter your username"
+              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring focus:border-[#B85C38]"
             />
-            {errors.username && <p className="text-red-500 text-xs mt-1">Username is required</p>}
           </div>
 
           <div className="mb-4">
@@ -67,27 +70,21 @@ const Login = () => {
               {...register("password", { required: true })}
               type="password"
               id="password"
-              placeholder="Enter password"
-              className="shadow appearance-none border border-[#9B1C1C] rounded w-full py-2 px-3 leading-tight focus:outline-none focus:ring-1 focus:ring-[#9B1C1C]"
+              placeholder="Enter your password"
+              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring focus:border-[#B85C38]"
             />
-            {errors.password && <p className="text-red-500 text-xs mt-1">Password is required</p>}
           </div>
+          {message && <p className="text-red-500 text-sm italic mb-3">{message}</p>}
 
-          {message && <p className="text-red-500 text-xs italic mb-3">{message}</p>}
-
-          <div>
-            <button
-              className="bg-[#9B1C1C] w-full hover:bg-[#7A1515] text-white font-bold py-2 rounded focus:outline-none transition"
-            >
-              Login
-            </button>
-          </div>
+          <button className="w-full bg-[#9B1C1C] text-white font-bold py-2 px-4 rounded-md hover:bg-[#B85C38] focus:outline-none">
+            Login
+          </button>
         </form>
 
-        <p className="mt-5 text-center text-[#9B1C1C] text-xs">©2025 Book Store. All rights reserved.</p>
+        <p className="mt-5 text-center text-gray-500 text-xs">©2025 Book Store. All rights reserved.</p>
       </div>
     </div>
   );
 };
 
-export default Login;
+export default AdminLogin;
