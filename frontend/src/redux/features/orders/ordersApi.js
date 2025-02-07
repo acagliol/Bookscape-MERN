b1748b567bc7790f5f8089757ad5bc8e21a33934
alejo <alejo.cagliolo@outlook.com>
2025-02-07 16:27:12 -0500

    fixing again

--- frontend/src/redux/features/orders/ordersApi.js
+++ frontend/src/redux/features/orders/ordersApi.js
@@ -1,6 +1,6 @@
 
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import getBaseUrl from '../../../utils/baseURL';
+import getBaseUrl from '../../../utils/baseURL.js';
 
 const ordersApi = createApi({
     reducerPath: 'ordersApi',
